eb01638ac4f3a864081e7f94f5a46f91d97c44da
akiosan0803 <wisdom.glory.passion.2030@gmail.com>
2019-09-23 22:08:11 +0900

    change code
    追加したら、同じユーザーが二回出てしまうので、訂正しました。

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -52,7 +52,7 @@ $(document).on('turbolinks:load', function(){
     $(this).parent().remove();
 
     var serch_user = $("#chat-group-user-22.chat-group-user.clearfix")
-    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+    var html = `<div class='chat-group-user'>
                   <input name='group[user_ids][]' type='hidden' value=${user_id}>
                   <p class='chat-group-user__name'>${user_name}</p>
                   <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,8 +19,6 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id]) 
-  
   end
   
 
