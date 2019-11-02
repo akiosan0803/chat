82f6ef9bcd14b768397250374a8d474a3f4e7aab
akiosan0803 <wisdom.glory.passion.2030@gmail.com>
2019-11-02 13:12:16 +0900

    change

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :message
-  has_many :group_users
-  has_many :groups, through: :group_users
+  has_many :group_user
+  has_many :group, through: :group_user
 end
